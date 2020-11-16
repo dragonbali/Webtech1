6152ac186f352f2fcbff6d3b1c1dd6abb5b4ef38
Hornyak <kicsikis@gmail.com>
2020-11-16 20:14:11 +0100

    basic files added(jquerry and index files)

---
+++ beadando/js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function (){
+    $("#content").load("lorem.html");
+
+    $.each($(".menuButton"), function(index, value){
+        $(value).click(function(event){
+            event.preventDefault();
+            if ($(this).find("a").attr("href") == "index.html") {
+                open("index.html", "_self");
+            }
+            else{
+                $("#content").load($(this).find("a").attr("href"));
+            }
+        })
+    })
+})
